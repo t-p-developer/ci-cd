44dda2c419fba163ec98f41741d08bd5d500781e
Tarmo <tarmo.petrovits@builderhead.io>
2023-05-28 08:35:43 +0300

    Test github action

--- src/app.test.ts
+++ src/app.test.ts
@@ -2,6 +2,6 @@ import { expect, it } from 'vitest';
 it(
     'should pass if the two numbers would add up correctly in a language other than JavaScript',
     () => {
-        expect(0.2 + 0.1).toBeCloseTo(0.3);
+        expect(0.2 + 0.1).toBeCloseTo(0.34);
     },
 );
